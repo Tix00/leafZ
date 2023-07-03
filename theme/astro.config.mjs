9ca0b201f3987a000b5ec8149bfc239a7e9d6ba7
Angelo Italiano <angeloitaliano00@gmail.com>
2023-07-03 22:49:09 +0200

    v2

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import lit from "@astrojs/lit";
-import react from "@astrojs/react";
+//import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
@@ -14,7 +14,7 @@ export default defineConfig({
   sitemap: true,
   outDir: './dist',
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit(), react()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), image(), lit(), /* react() */], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
